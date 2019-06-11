9d02cb9975ddd988e1f7c9144dfe156f2c9a80da
stiliyan <stiliqn.goranov@gmail.com>
2019-06-11 14:01:49 +0300

    add test for isHTMLResource

--- src/test/scala/Test.scala
+++ src/test/scala/Test.scala
@@ -1,10 +1,28 @@
 package homework3
 
+import java.util.concurrent.Executors
+
+import homework3.http.AsyncHttpClient
 import org.scalatest.{FlatSpec, Matchers}
 
+import scala.concurrent.duration.Duration
+import scala.concurrent.{Await, ExecutionContext}
+import scala.util.{Failure, Success}
+
 class Test extends FlatSpec with Matchers {
-  // TODO: add meaningful unit tests!
-  "true" should "be true" in {
-    true shouldBe true
+
+  val httpClient = new AsyncHttpClient
+  val threadPool = Executors.newFixedThreadPool(32)
+  implicit val ec = ExecutionContext.fromExecutor(threadPool)
+
+  "response from HTTP GET request from https://www.google.com/" should "be html resource" in {
+    val futureResponse = httpClient.get("https://www.google.com/")
+
+    futureResponse onComplete {
+      case Success(response) => response.isHTMLResource shouldBe true
+      case Failure(_) => fail("Failed HTTP GET request!")
+    }
+
+    Await.ready(futureResponse, Duration.Inf)
   }
 }
