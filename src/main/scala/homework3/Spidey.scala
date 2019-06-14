bd6f1c34786fac34f916b49b5030a11af4de99b7
stiliyan <stiliqn.goranov@gmail.com>
2019-06-14 17:55:48 +0300

    make crawl functional instead of procedural

--- src/main/scala/homework3/Spidey.scala
+++ src/main/scala/homework3/Spidey.scala
@@ -4,7 +4,7 @@ import homework3.html.HtmlUtils
 import homework3.http._
 import homework3.math.Monoid
 
-import scala.collection.mutable
+import scala.collection.immutable.HashSet
 import scala.concurrent.duration.Duration
 import scala.concurrent.{Await, ExecutionContext, Future}
 
@@ -21,38 +21,49 @@ class Spidey(httpClient: HttpClient)(implicit ex: ExecutionContext) {
       throw new IllegalArgumentException("maxDepth cannot be less than zero!")
     }
 
-    var visited: mutable.HashSet[String] = mutable.HashSet.empty
-
-    var toVisit: mutable.Queue[String] = mutable.Queue.empty
-    toVisit.enqueue(url)
-
-    var results: mutable.MutableList[O] = mutable.MutableList.empty
-
-    var curDepth = config.maxDepth
-
-    while (toVisit.nonEmpty) {
-      curDepth -= 1
-      visited ++= toVisit
-
-      val urlAndHttpResponseTuple = toVisit.map(url => (url, httpClient.get(url))).map(tuple => (tuple._1, Await.result(tuple._2, Duration.Inf)))
-
-      toVisit.clear
-
-      // only add new links to visit if depth is still >= 0
-      if (curDepth >= 0) {
-        urlAndHttpResponseTuple.foreach(tuple => {
-          if (tuple._2.isHTMLResource) {
-            toVisit ++= HtmlUtils.linksOf(tuple._1, tuple._2.body).filter(!visited(_))
-          }
-        })
+    def crawlRecHelper(visited: HashSet[String], toVisit: List[String], curResult: O, curDepth: Int): O = {
+      if (toVisit.isEmpty) {
+        curResult
       }
+      else {
+        // do get requests on all URLs and wait for responses
+        val urlAndHttpResponseTuple =
+          toVisit
+            .map(url => (url, httpClient.get(url)))
+            .map(tuple => (tuple._1, Await.result(tuple._2, Duration.Inf)))
 
-      results +=
-        urlAndHttpResponseTuple.map(tuple => processor(tuple._1, tuple._2))
+        // process all the responses and wait for result
+        val resultFromProcessing = urlAndHttpResponseTuple.map(tuple => processor(tuple._1, tuple._2))
           .map(Await.result(_, Duration.Inf))
           .foldLeft(implicitly[Monoid[O]].identity)(implicitly[Monoid[O]].op(_, _))
+
+        // TODO SG: As soon as response is received from a given url, start the processor.
+        // (Currently we wait for all responses to finish and then begin processing them)
+
+        if (curDepth > 0) {
+          crawlRecHelper(
+            visited ++ toVisit,
+            urlAndHttpResponseTuple.flatMap(pair => {
+              if (pair._2.isHTMLResource) {
+                HtmlUtils.linksOf(pair._2.body, pair._1).filter(!visited(_))
+              } else {
+                List.empty
+              }
+            }),
+            Monoid[O].op(resultFromProcessing, curResult),
+            curDepth - 1)
+        }
+        else {
+          crawlRecHelper(
+            HashSet.empty,
+            List.empty,
+            Monoid[O].op(resultFromProcessing, curResult),
+            curDepth - 1
+          )
+        }
+      }
     }
 
-    results.foldLeft(implicitly[Monoid[O]].identity)(implicitly[Monoid[O]].op(_, _))
+    crawlRecHelper(HashSet.empty, List(url), Monoid[O].identity, config.maxDepth)
   }
 }
