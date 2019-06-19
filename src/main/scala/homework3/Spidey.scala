6c41d801881dae671f865e2d52061b1bb27e4244
stiliyan <stiliqn.goranov@gmail.com>
2019-06-19 21:06:40 +0300

    Fix: start processing http response as soon as it is recieved

--- src/main/scala/homework3/Spidey.scala
+++ src/main/scala/homework3/Spidey.scala
@@ -6,8 +6,10 @@ import homework3.math.Monoid
 
 import scala.annotation.tailrec
 import scala.collection.immutable.HashSet
+import scala.collection.mutable
 import scala.concurrent.duration.Duration
 import scala.concurrent.{Await, ExecutionContext, Future}
+import scala.util.Success
 
 case class SpideyConfig(maxDepth: Int,
                         sameDomainOnly: Boolean = true,
@@ -17,7 +19,6 @@ case class SpideyConfig(maxDepth: Int,
 class Spidey(httpClient: HttpClient)(implicit ex: ExecutionContext) {
   def crawl[O: Monoid](url: String, config: SpideyConfig)
                       (processor: Processor[O]): Future[O] = Future {
-
     if (config.maxDepth < 0) {
       throw new IllegalArgumentException("maxDepth cannot be less than zero!")
     }
@@ -30,38 +31,33 @@ class Spidey(httpClient: HttpClient)(implicit ex: ExecutionContext) {
         curResult
       }
       else {
-        // do get requests on all URLs and wait for responses
-        val urlAndHttpResponseTuple =
-          toVisit
-            .map(url => (url, httpClient.get(url)))
-            .map(tuple => (tuple._1, Await.result(tuple._2, Duration.Inf)))
+        val urlToResponseMap: mutable.Map[String, HttpResponse] = mutable.Map.empty
 
-        // process all the responses and wait for result
-        val resultFromProcessing = urlAndHttpResponseTuple.map(tuple => processor(tuple._1, tuple._2))
+        val result = toVisit.map(url =>
+          (httpClient.get(url) andThen {
+            case Success(httpResponse) => urlToResponseMap += url -> httpResponse
+          }).flatMap(processor(url, _)))
           .map(Await.result(_, Duration.Inf))
           .foldLeft(Monoid[O].identity)(_ |+| _)
-
-        // TODO SG: As soon as response is received from a given url, start the processor.
-        // (Currently we wait for all responses to finish and then begin processing them)
 
         if (curDepth > 0) {
           crawlRecHelper(
             visited ++ toVisit,
-            urlAndHttpResponseTuple.flatMap(pair => {
-              if (pair._2.isHTMLResource) {
-                HtmlUtils.linksOf(pair._2.body, pair._1).filter(!visited(_))
+            urlToResponseMap.toList.flatMap(keyValuePair => {
+              if (keyValuePair._2.isHTMLResource) {
+                HtmlUtils.linksOf(keyValuePair._2.body, keyValuePair._1).filter(!visited(_))
               } else {
                 List.empty
               }
             }),
-            resultFromProcessing |+| curResult,
+            result |+| curResult,
             curDepth - 1)
         }
         else {
           crawlRecHelper(
             HashSet.empty,
             List.empty,
-            resultFromProcessing |+| curResult,
+            result |+| curResult,
             curDepth - 1
           )
         }
