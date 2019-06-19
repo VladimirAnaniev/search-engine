2a8b756976b0493d823b231ff9fd96db45509c46
stiliyan <stiliqn.goranov@gmail.com>
2019-06-19 10:44:53 +0300

    add unit tests for crawl and wordCounter

--- src/test/scala/MockHttpClient.scala
+++ src/test/scala/MockHttpClient.scala
@@ -18,24 +18,34 @@ class MockHttpResponseFactory {
 }
 
 class MockHttpClient extends HttpClient {
-
-  val mockHttpResponseFactory = new MockHttpResponseFactory
-
-  val testResponse1 =
-    mockHttpResponseFactory.createResponse("<a href=\"https://www.test1.com/\">Visit our HTML tutorial</a>")
-
-  val testResponse2 =
-    mockHttpResponseFactory.createResponse("<a href=\"https://www.test2.com/\">Visit our HTML tutorial</a>")
-
-  val testResponse3 =
-    mockHttpResponseFactory.createResponse("<a href=\"https://www.test3.com/\">Visit our HTML tutorial</a>")
-
   def get(url: String): Future[HttpResponse] = url match {
-    case "https://www.test1.com/" => Promise[HttpResponse].complete(Try(testResponse2)).future
-    case "https://www.test2.com/" => Promise[HttpResponse].complete(Try(testResponse3)).future
-    case "https://www.test3.com/" => Promise[HttpResponse].complete(Try(testResponse1)).future
+    case "https://www.test1.com/" => Promise[HttpResponse].complete(Try(MockHttpClient.testResponse2)).future
+    case "https://www.test2.com/" => Promise[HttpResponse].complete(Try(MockHttpClient.testResponse3)).future
+    case "https://www.test3.com/" => Promise[HttpResponse].complete(Try(MockHttpClient.testResponse1)).future
     case _ => throw new IllegalArgumentException("Wrong testing url!")
   }
+}
+
+object MockHttpClient {
+  val mockHttpResponseFactory = new MockHttpResponseFactory
+
+  val textResponse1 = "text-response-1"
+  val linksResponse1 = List("https://www.test2.com/")
+
+  val textResponse2 = "text-response-2"
+  val linksResponse2 = List("https://www.test3.com/")
+
+  val textResponse3 = "text-response-3"
+  val linksResponse3 = List("https://www.test1.com/")
+
+  val testResponse1: HttpResponse =
+    mockHttpResponseFactory.createResponse(s""""<a href="https://www.test1.com/">$textResponse1</a>"""")
+
+  val testResponse2: HttpResponse =
+    mockHttpResponseFactory.createResponse(s""""<a href="https://www.test2.com/">$textResponse2</a>"""")
+
+  val testResponse3: HttpResponse =
+    mockHttpResponseFactory.createResponse(s""""<a href="https://www.test3.com/">$textResponse3</a>"""")
 }
 
 
--- src/test/scala/Test.scala
+++ src/test/scala/Test.scala
@@ -7,16 +7,19 @@ import homework3.math.Monoid._
 import homework3.processors.{WordCount, WordCounter}
 import org.scalatest.{FlatSpec, Matchers}
 
-import scala.concurrent.Await
+import scala.concurrent.{Await, Future}
 import scala.concurrent.duration.Duration
 import scala.concurrent.ExecutionContext.Implicits.global
 
 class Test extends FlatSpec with Matchers {
+
+  def getFutureResultBlocking[R](f: Future[R]) = Await.result(f, Duration.Inf)
+
   val mockHttpClient = new MockHttpClient
   val testSpidey = new Spidey(mockHttpClient)
 
   "response from MockHttpClient" should "be html resource containing valid http link" in {
-    val httpResponse = Await.result(mockHttpClient.get("https://www.test1.com/"), Duration.Inf)
+    val httpResponse = getFutureResultBlocking(mockHttpClient.get("https://www.test1.com/"))
 
     httpResponse.isSuccess shouldBe true
 
@@ -27,8 +30,8 @@ class Test extends FlatSpec with Matchers {
     links.size shouldBe 1
     links.foreach(HttpUtils.isValidHttp(_) shouldBe true)
     links.head shouldBe "https://www.test2.com/"
-    WordCount.wordsOf(httpResponse.body) shouldBe
-      List("a", "href", "https", "www", "test2", "com", "Visit", "our", "HTML", "tutorial", "a")
+    WordCount.wordsOf(HtmlUtils.toText(httpResponse.body)) shouldBe
+      List("text", "response", "2")
   }
 
   "wordCountMonoid" should "add two WordCount instances correctly" in {
@@ -39,18 +42,38 @@ class Test extends FlatSpec with Matchers {
       WordCount(Map("a" -> 2, "b" -> 3, "c" -> 4))
   }
 
-  "crawl" should "go to only one link when depth is 0" in {
-    // testSpidey.crawl("https://www.test1.com/", SpideyConfig(0))(WordCounter)
+  "processors should work correctly and crawl" should "go to only one link when depth is 0" in {
+    getFutureResultBlocking(testSpidey.crawl("https://www.test1.com/", SpideyConfig(0))(WordCounter)).wordToCount shouldBe
+      Map("text" -> 1, "response" -> 1, "2" -> 1)
   }
 
+  it should "go to two links when depth is 1" in {
+    getFutureResultBlocking(testSpidey.crawl("https://www.test1.com/", SpideyConfig(1))(WordCounter)).wordToCount shouldBe
+      Map("text" -> 2, "response" -> 2, "2" -> 1, "3" -> 1)
+  }
+
+  it should "go to three links when depth is 2" in {
+    getFutureResultBlocking(testSpidey.crawl("https://www.test1.com/", SpideyConfig(2))(WordCounter)).wordToCount shouldBe
+      Map("text" -> 3, "response" -> 3, "2" -> 1, "3" -> 1, "1" -> 1)
+  }
+
+  it should "go to all the links when depth is a big number" in {
+    getFutureResultBlocking(testSpidey.crawl("https://www.test1.com/", SpideyConfig(1000))(WordCounter)).wordToCount shouldBe
+      Map("text" -> 3, "response" -> 3, "2" -> 1, "3" -> 1, "1" -> 1)
+  }
 
 }
 
 object Test extends App {
+  def getFutureResultBlocking[R](f: Future[R]) = Await.result(f, Duration.Inf)
+
   val mockHttpClient = new MockHttpClient
 
   import homework3.processors.WordCount
 
-  println(WordCount.wordsOf(Await.result(mockHttpClient.get("https://www.test1.com/"), Duration.Inf).body))
+  val httpResponse = getFutureResultBlocking(mockHttpClient.get("https://www.test1.com/"))
 
+  val links = HtmlUtils.linksOf(httpResponse.body, "https://www.test1.com/")
+
+  println(WordCount.wordsOf(HtmlUtils.toText(httpResponse.body)))
 }
