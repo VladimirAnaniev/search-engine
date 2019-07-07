c8d281db859b6878b0ca1c5b94b20edbe46a5f03
Vladimir Ananiev <vvananiev@uni-sofia.bg>
2019-07-07 12:45:27 -0700

    Make hyperlinks clickable, add styles

--- app/controllers/ApplicationController.scala
+++ app/controllers/ApplicationController.scala
@@ -1,7 +1,7 @@
 package controllers
 
 import play.api.libs.ws.WSClient
-import play.api.mvc.{AbstractController, ControllerComponents}
+import play.api.mvc.{AbstractController, Action, AnyContent, ControllerComponents}
 import services.SearchService
 
 import scala.concurrent.ExecutionContext
@@ -12,11 +12,11 @@ class ApplicationController(cc: ControllerComponents,
   val searchService = SearchService()
 
   // Can be both def or val
-  def index = Action { request =>
+  def index: Action[AnyContent] = Action { request =>
     Ok(views.html.index(List.empty))
   }
 
-  def search(keyword: String) = Action.async {
+  def search(keyword: String): Action[AnyContent] = Action.async {
     searchService.search(keyword).map(res => Ok(views.html.index(res.toList)))
   }
 }
